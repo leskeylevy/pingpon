e8320082887c58f9b25d67805bf24b7bbf9ffcfe
leskeylevy <leskeylevy@gmail.com>
2018-02-26 06:59:21 +0300

    adding javascripts

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+//userinterface logic
+function pingPong(num) {
+  var myArray = [];
+  for (var a = 1; a <= num; a++) {
+    if (a % 15 === 0) {
+      myArray.push("PING PONG");
+    } else if (a % 5 === 0) {
+      myArray.push("PONG");
+    } else if (a % 3 === 0) {
+      myArray.push("PING");
+    } else {
+      myArray.push(a);
+    }
+  };
+  return myArray;
+};
+//business logic
+$(document).ready(function() {
+  $("form#input-num").submit(function(event) {
+    event.preventDefault();
+    $("li.res").empty();
+    var num = parseInt($("#number-input").val());
+    var derivedArray = pingPong(num) + '';
+    var splitArray = derivedArray.split(",").join("<br>");
+    $("div.result-table").addClass('res').append("<li class='res'>" + splitArray + "</li>");
+  });
+});
